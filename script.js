144b4bbd2de6689a449b7a9b8d884da8d97401e4
Krishnareddy-1807 <krishnareddyyaramala24@gmail.com>
2025-05-12 10:26:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
- 
+	let charCount = {}
+	for(let char of str ){
+		charCount[char] = (charCount[char] || 0) + 1
+	}
+
+	for(let char of str ){
+		if(charCount[char] ===1)
+	}
+	return char
 }
 const input = prompt("krishna reddy");
 alert(firstNonRepeatedChar(input)); 
